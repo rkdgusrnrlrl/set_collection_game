2fa3a9888bfa721684345c5e1100ba67e185c1a2
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-07-16 18:04:26 +0900

    refac : MyHand 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <Card :card="card"/>
+
+    <MyHand :cards="cards"/>
+
     <draggable element="ul" class="dragArea" :list="list1" :options="{group:'people'}" style="border: 1px solid black">
       <li v-for="(element, index) in list1" :key="element.id">
         {{element.name}} {{index}}
@@ -16,17 +19,24 @@
 </template>
 
 <script>
-import Card from './components/Card.vue'
+import Card from './components/Card'
+import MyHand from './components/MyHand'
 import draggable from 'vuedraggable'
 
 export default {
   name: 'app',
   components: {
     Card,
+    MyHand,
     draggable
   },
   data() {
       return {
+          cards : [
+              {name:"Joao", id:6, imgUrl : "img"},
+              {name:"Joao", id:7, imgUrl : "img"},
+              {name:"Joao", id:8, imgUrl : "img"}
+          ],
           card : {name:"Joao", id:2, imgUrl : "img"},
           list1:[{name:"John", id:1},
               {name:"Joao", id:3},
